1ffee9911f60bbf0c629281d9948e4b04770a863
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-01 11:15:26 +0200

    feat: created model and serializer, added router to urls and view, made migrations and server is running

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
