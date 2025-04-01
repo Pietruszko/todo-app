04a73e1e6b50ff9e343bf11239886fd410f06ffa
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-01 12:40:07 +0200

    feat: created simple filter for tasks

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import Task
 from .serializers import TaskSerializer
+from .filters import TaskFilter
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TaskFilter
