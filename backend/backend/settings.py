2c53f4d46983a948d4eb236fa635daae2f5e02c9
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-03 12:36:07 +0200

    fix: added render origins to cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -155,6 +156,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",  # Production
     "http://127.0.0.1:8080",
     "http://frontend:80",  # Frontend
+    "https://todo-app-5v21.onrender.com/",  # Render
 ]
 
 CORS_ALLOW_CREDENTIALS = True
